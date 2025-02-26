3f81fe9b1a2ae6da2af5c4f994c2033e6a6645ff
tnchnkit@hotmail.com <kelvingssb@gmail.com>
2025-02-26 16:39:58 +0800

    Revert "fix"
    This reverts commit 79fb26dd0265bcbfda8e3b8809830e8ebaf920e3.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,1 @@
-// .vite.d.ts or vite.d.ts
-declare global {
-  namespace JSX {
-    interface IntrinsicElements {
-      [key: string]: React.ReactNode
-    }
-  }
-}
-
-export {} // This ensures that the file is treated as a module
+/// <reference types="vite/client" />
